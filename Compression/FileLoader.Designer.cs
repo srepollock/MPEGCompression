9e800557026a2493024fc171e01d75e2aa18b26d
unknown <a00924319@SE12L0308-13.ad.bcit.ca>
2016-02-10 08:28:11 -0800

    updated ui buttons

--- Compression/FileLoader.Designer.cs
+++ Compression/FileLoader.Designer.cs
@@ -29,24 +29,24 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.fileNameBox = new System.Windows.Forms.TextBox();
+            this.rgbChangeButton = new System.Windows.Forms.Button();
+            this.showYCbCrButton = new System.Windows.Forms.Button();
+            this.ShowYButton = new System.Windows.Forms.Button();
             this.showCbButton = new System.Windows.Forms.Button();
             this.ShowCrButton = new System.Windows.Forms.Button();
-            this.ShowYButton = new System.Windows.Forms.Button();
-            this.rgbChangeButton = new System.Windows.Forms.Button();
-            this.fileNameBox = new System.Windows.Forms.TextBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.showYCbCrButton = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.panel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -55,108 +55,127 @@
             this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Controls.Add(this.pictureBox3, 0, 1);
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 11F));
             this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox3, 1, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 38);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1947, 1009);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(983, 524);
             this.tableLayoutPanel1.TabIndex = 0;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox3.Location = new System.Drawing.Point(976, 507);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(968, 499);
-            this.pictureBox3.TabIndex = 3;
-            this.pictureBox3.TabStop = false;
             // 
             // pictureBox2
             // 
             this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox2.Location = new System.Drawing.Point(976, 3);
+            this.pictureBox2.Location = new System.Drawing.Point(493, 2);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(968, 498);
+            this.pictureBox2.Size = new System.Drawing.Size(488, 258);
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(2, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(967, 498);
+            this.pictureBox1.Size = new System.Drawing.Size(487, 258);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // panel1
+            // flowLayoutPanel1
             // 
-            this.panel1.Controls.Add(this.showYCbCrButton);
-            this.panel1.Controls.Add(this.showCbButton);
-            this.panel1.Controls.Add(this.ShowCrButton);
-            this.panel1.Controls.Add(this.ShowYButton);
-            this.panel1.Controls.Add(this.rgbChangeButton);
-            this.panel1.Controls.Add(this.fileNameBox);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(3, 507);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(967, 499);
-            this.panel1.TabIndex = 2;
+            this.flowLayoutPanel1.Controls.Add(this.fileNameBox);
+            this.flowLayoutPanel1.Controls.Add(this.rgbChangeButton);
+            this.flowLayoutPanel1.Controls.Add(this.showYCbCrButton);
+            this.flowLayoutPanel1.Controls.Add(this.ShowYButton);
+            this.flowLayoutPanel1.Controls.Add(this.showCbButton);
+            this.flowLayoutPanel1.Controls.Add(this.ShowCrButton);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 265);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(485, 256);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // fileNameBox
+            // 
+            this.fileNameBox.Location = new System.Drawing.Point(2, 2);
+            this.fileNameBox.Margin = new System.Windows.Forms.Padding(2);
+            this.fileNameBox.Name = "fileNameBox";
+            this.fileNameBox.ReadOnly = true;
+            this.fileNameBox.Size = new System.Drawing.Size(275, 20);
+            this.fileNameBox.TabIndex = 6;
+            // 
+            // rgbChangeButton
+            // 
+            this.rgbChangeButton.Location = new System.Drawing.Point(281, 2);
+            this.rgbChangeButton.Margin = new System.Windows.Forms.Padding(2);
+            this.rgbChangeButton.Name = "rgbChangeButton";
+            this.rgbChangeButton.Size = new System.Drawing.Size(95, 31);
+            this.rgbChangeButton.TabIndex = 7;
+            this.rgbChangeButton.Text = "Data Change";
+            this.rgbChangeButton.UseVisualStyleBackColor = true;
+            this.rgbChangeButton.Click += new System.EventHandler(this.rgbChangeButton_Click);
+            // 
+            // showYCbCrButton
+            // 
+            this.showYCbCrButton.Location = new System.Drawing.Point(380, 2);
+            this.showYCbCrButton.Margin = new System.Windows.Forms.Padding(2);
+            this.showYCbCrButton.Name = "showYCbCrButton";
+            this.showYCbCrButton.Size = new System.Drawing.Size(95, 31);
+            this.showYCbCrButton.TabIndex = 11;
+            this.showYCbCrButton.Text = "Show YCbCr";
+            this.showYCbCrButton.UseVisualStyleBackColor = true;
+            this.showYCbCrButton.Click += new System.EventHandler(this.showYCbCrButton_Click);
+            // 
+            // ShowYButton
+            // 
+            this.ShowYButton.Location = new System.Drawing.Point(2, 37);
+            this.ShowYButton.Margin = new System.Windows.Forms.Padding(2);
+            this.ShowYButton.Name = "ShowYButton";
+            this.ShowYButton.Size = new System.Drawing.Size(95, 31);
+            this.ShowYButton.TabIndex = 8;
+            this.ShowYButton.Text = "Show Y";
+            this.ShowYButton.UseVisualStyleBackColor = true;
+            this.ShowYButton.Click += new System.EventHandler(this.ShowYButton_Click);
             // 
             // showCbButton
             // 
-            this.showCbButton.Location = new System.Drawing.Point(675, 276);
+            this.showCbButton.Location = new System.Drawing.Point(101, 37);
+            this.showCbButton.Margin = new System.Windows.Forms.Padding(2);
             this.showCbButton.Name = "showCbButton";
-            this.showCbButton.Size = new System.Drawing.Size(174, 58);
-            this.showCbButton.TabIndex = 4;
+            this.showCbButton.Size = new System.Drawing.Size(95, 31);
+            this.showCbButton.TabIndex = 10;
             this.showCbButton.Text = "Show Cb";
             this.showCbButton.UseVisualStyleBackColor = true;
             this.showCbButton.Click += new System.EventHandler(this.showCbButton_Click);
             // 
             // ShowCrButton
             // 
-            this.ShowCrButton.Location = new System.Drawing.Point(675, 178);
+            this.ShowCrButton.Location = new System.Drawing.Point(200, 37);
+            this.ShowCrButton.Margin = new System.Windows.Forms.Padding(2);
             this.ShowCrButton.Name = "ShowCrButton";
-            this.ShowCrButton.Size = new System.Drawing.Size(174, 58);
-            this.ShowCrButton.TabIndex = 3;
+            this.ShowCrButton.Size = new System.Drawing.Size(95, 31);
+            this.ShowCrButton.TabIndex = 9;
             this.ShowCrButton.Text = "Show Cr";
             this.ShowCrButton.UseVisualStyleBackColor = true;
             this.ShowCrButton.Click += new System.EventHandler(this.ShowCrButton_Click);
             // 
-            // ShowYButton
+            // pictureBox3
             // 
-            this.ShowYButton.Location = new System.Drawing.Point(675, 86);
-            this.ShowYButton.Name = "ShowYButton";
-            this.ShowYButton.Size = new System.Drawing.Size(174, 58);
-            this.ShowYButton.TabIndex = 2;
-            this.ShowYButton.Text = "Show Y";
-            this.ShowYButton.UseVisualStyleBackColor = true;
-            this.ShowYButton.Click += new System.EventHandler(this.ShowYButton_Click);
-            // 
-            // rgbChangeButton
-            // 
-            this.rgbChangeButton.Location = new System.Drawing.Point(10, 40);
-            this.rgbChangeButton.Name = "rgbChangeButton";
-            this.rgbChangeButton.Size = new System.Drawing.Size(174, 58);
-            this.rgbChangeButton.TabIndex = 1;
-            this.rgbChangeButton.Text = "Data Change";
-            this.rgbChangeButton.UseVisualStyleBackColor = true;
-            this.rgbChangeButton.Click += new System.EventHandler(this.rgbChangeButton_Click);
-            // 
-            // fileNameBox
-            // 
-            this.fileNameBox.Location = new System.Drawing.Point(4, 4);
-            this.fileNameBox.Name = "fileNameBox";
-            this.fileNameBox.ReadOnly = true;
-            this.fileNameBox.Size = new System.Drawing.Size(501, 29);
-            this.fileNameBox.TabIndex = 0;
+            this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox3.Location = new System.Drawing.Point(494, 265);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(486, 256);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
             // 
             // menuStrip1
             // 
@@ -165,7 +184,8 @@
             this.fileToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1947, 38);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(3, 1, 0, 1);
+            this.menuStrip1.Size = new System.Drawing.Size(983, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -174,42 +194,33 @@
             this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.loadToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(56, 34);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 22);
             this.fileToolStripMenuItem.Text = "&File";
             // 
             // loadToolStripMenuItem
             // 
             this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
-            this.loadToolStripMenuItem.Size = new System.Drawing.Size(150, 34);
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
             this.loadToolStripMenuItem.Text = "&Load";
             this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
             // 
-            // showYCbCrButton
-            // 
-            this.showYCbCrButton.Location = new System.Drawing.Point(675, 371);
-            this.showYCbCrButton.Name = "showYCbCrButton";
-            this.showYCbCrButton.Size = new System.Drawing.Size(174, 58);
-            this.showYCbCrButton.TabIndex = 5;
-            this.showYCbCrButton.Text = "Show YCbCr";
-            this.showYCbCrButton.UseVisualStyleBackColor = true;
-            this.showYCbCrButton.Click += new System.EventHandler(this.showYCbCrButton_Click);
-            // 
             // FileLoader
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1947, 1047);
+            this.ClientSize = new System.Drawing.Size(983, 548);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FileLoader";
             this.Text = "Form1";
             this.tableLayoutPanel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -222,17 +233,17 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.TextBox fileNameBox;
         private System.Windows.Forms.Button rgbChangeButton;
-        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button showYCbCrButton;
+        private System.Windows.Forms.Button ShowYButton;
         private System.Windows.Forms.Button showCbButton;
         private System.Windows.Forms.Button ShowCrButton;
-        private System.Windows.Forms.Button ShowYButton;
-        private System.Windows.Forms.Button showYCbCrButton;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
 
